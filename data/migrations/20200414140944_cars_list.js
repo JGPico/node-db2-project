9e3f6303a2aef06be3485b4579582f37c0225837
JGPico <minq_2@hotmail.com>
2020-04-14 14:37:32 -0600

    MVP

---
+++ data/migrations/20200414140944_cars_list.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id');
+
+    tbl.string('VIN', 128)
+    .notNullable()
+    .unique()
+    .index();
+
+    tbl.string('make', 128)
+    .notNullable();
+
+    tbl.string('model', 128)
+    .notNullable();
+
+    tbl.integer('mileage')
+    .notNullable();
+
+    tbl.string('transmission', 128);
+
+    tbl.string('status', 128);
+
+  });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars');
+};
